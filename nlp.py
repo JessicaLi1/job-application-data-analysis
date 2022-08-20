f7c3e976e381c31110f74cded214b0ef5d9ec1a4
Jessica Li <90886536+JessicaLi1@users.noreply.github.com>
2022-08-20 21:11:41 +0100

    new file

---
+++ nlp.py
@@ -0,0 +1,2 @@
+text1=open(https://vscode.dev/github/JessicaLi1/job-application-data-analysis/blob/81fccf670dc9b93a184fc0e3ffcc201feeff975f/Texts%20from%20Logical%20Reasoning%20Test%20Solutions#L1)
+
